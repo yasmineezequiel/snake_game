d9d98153264ce96d89b69bc720c38cf821171a62
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2020-01-17 17:40:48 +0100

    Creating unit box load food and canvas img Created the snake Adding score adding some style in js.

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const cvs = document.getElementById("snake");
+const ctx = cvs.getContext("2d");
+
+// create the unit
+const box = 32;
+
+// load the image
+
+const ground = new Image();
+ground.src = "/img/ground.png";
+
+const foodImg = new Image();
+foodImg.src = "/img/orange.png";
+
+// create a snake
+
+let snake = [];
+snake[0] = {
+    x : 9 * box,
+    y : 10 * box
+}
+
+// create the food
+
+let food = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+
+// create score
+
+let score = 0;
+
+//function that will draw everything into the canvas 
+
+function draw(){
+    ctx.drawImage(ground,0,0);
+    for( let i = 0; i < snake.length; i++){
+        ctx.fillStyle = ( i == 0)? "green" : "white"
+        ctx.fillRect(snake[i].x,snake[i].y,box,box);
+
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
+    }
+}
+// call draw function every 100 ms
+
+let game = setInterval(draw,100);
